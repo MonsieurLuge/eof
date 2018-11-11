ac15eb5618d3709ce12bf51e1a98912427a3bf19
monsieurluge <monsieurluge@gmail.com>
2018-11-11 23:06:10 +0100

    update the composer + index files

--- index.php
+++ index.php
@@ -3,12 +3,4 @@
 require_once __DIR__ . '/vendor/autoload.php';
 ini_set('display_errors', 1);
 
-use EOF\HTTP\Response\BaseResponse;
-use Website\Demo\Website;
-use Website\Demo\Homepage;
-
-$website = new Website(
-    new Homepage()
-);
-
-$website->sendThrough(new BaseResponse());
+// todo
